92f2aa636d59d4488e97eae74fc0977857c26976
krkyk <madeinpink1111@gmail.com>
2022-04-01 13:48:53 +0000

    [progress]follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,23 @@
 class RelationshipsController < ApplicationController
-  
+
   def create
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
-  
+
   def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.refere
   end
-  
+
   def followings
+    user=User.find(params[:user_id])
+    @users=user.followings
   end
-  
+
   def followers
+    user=User.find(params[:user_id])
+    @users=user.followers
   end
-  
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "c99f0a82887f4bbebd823ce1e9d511b9.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "7f26772328724b9c9ec5882b45478728.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
