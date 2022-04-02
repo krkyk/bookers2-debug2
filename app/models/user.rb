bced84ded04a37beee064531eab85963f4de210b
krkyk <madeinpink1111@gmail.com>
2022-04-02 08:40:48 +0000

    [Finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
   end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -41,6 +41,21 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+
+  def self.looks(search,word)
+    if search=="perfect_match"
+      @user=User.where("name LIKE?","#{word}")
+    elsif search=="forward_match"
+      @user=User.where("name LIKE?","#{word}%")
+    elsif search=="backward_match"
+      @user=User.where("name LIKE?","%#{word}")
+    elsif search=="partial_match"
+      @user=User.where("name LIKE?","%#{word}%")
+    else
+      @user=User.all
+    end
+  end
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     get "followings"=>"relationships#followings",as:"followings"
     get "followers"=>"relationships#followers",as:"followers"
   end
-
+  get "search"=>"searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
