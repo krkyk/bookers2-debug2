7812fba6bb3a037f6e8512a2b22b283bf19e178f
krkyk <madeinpink1111@gmail.com>
2022-04-08 06:57:58 +0000

    [Finish]group-making2

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,10 @@
 class Group < ApplicationRecord
-  has_many :group_users
+  has_many :group_users,dependent: :destroy
   has_many :users,through: :group_users
   has_one_attached :group_image
 
   validates :name,presence: true
   validates :introduction,presence: true
-  #attachment :image,destroy: false
 
   def get_group_image(width, height)
   unless group_image.attached?
